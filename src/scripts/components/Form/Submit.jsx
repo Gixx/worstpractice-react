a5b6a83b8193a768dcbd1db3689fbc539809db40
Gabor Ivan <gabor.ivan@westwing.de>
2020-11-03 16:22:54 +0100

    Add states and styles

--- src/scripts/components/Form/Submit.jsx
+++ src/scripts/components/Form/Submit.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 class Submit extends Component
 {
     render() {
-        let enabled = this.props.data === 'yes';
+        let enabled = this.props.data.toLowerCase() === 'yes';
         return <button type="submit" style={{ backgroundColor: enabled ? 'green' : 'red'}}>
             { enabled ? 'OK' : 'Cancel' }
         </button>
